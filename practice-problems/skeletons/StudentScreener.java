c0c971b0fb3fecb12cb116cfee32ec1d1ee1427a
Khunt Miral <20ce051.miral.khunt@vvpedulink.ac.in>
2023-03-01 19:00:29 +0530

    StudentScreener

--- practice-problems/skeletons/StudentScreener.java
+++ practice-problems/skeletons/StudentScreener.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+
 public class StudentScreener {
   public static void main(String[] args) throws Exception {
     int totalStudents = 7;
@@ -13,7 +18,67 @@ public class StudentScreener {
   }
 
   public static String getEligibleStudents(int totalStudents, String[] students, int[] marks, String[] examResults, int totalMarks, float requiredPercentile) throws Exception {
-    //TODO: Implement this function and remove below line, which throws exception
-    throw new RuntimeException("Not Implemented");
+        List<Student> ans = new ArrayList<>();
+    StringBuilder sb = new StringBuilder();
+    sb.append("\"");
+    //storing marks, students and examResults in form of object so that operations can be performed easily
+    for(int i = 0; i < totalStudents; i++) {
+      Student std = new Student(marks[i],students[i],examResults[i]);
+      ans.add(std);
+    }
+    //sorted list based on marks
+    List<Student> sorted = ans.stream().sorted(new StudentMarksComparator()).toList();
+    for(int i = 0; i < sorted.size(); i++) {
+      //setting percentile of each student
+      sorted.get(i).setPtile(percentile(totalStudents,i+1));
+      //checking the required conditions and adding required result
+      if(sorted.get(i).ptile >= requiredPercentile && sorted.get(i).getExamResult().equalsIgnoreCase("Passed")) {
+        if(sb.length() > 2) sb.append(",");
+        sb.append(sorted.get(i).getName());
+      }
+    }
+    //adding last double quote for final result
+    sb.append("\"");
+    return sb.toString();
+  }
+  //for calculating percentile
+  static float percentile(int n, int i) {
+    return ((float) n -  (float) i)/ (float) n * 100;
+  }
+  static class Student {
+    private int marks;
+    private String name;
+    private String examResult;
+    private float ptile;
+
+    Student(int marks, String name, String result) {
+      this.marks=marks;
+      this.name=name;
+      this.examResult=result;
+    }
+    Student(){}
+    public int getMarks() {
+      return marks;
+    }
+
+    public String getName() {
+      return name;
+    }
+    public String getExamResult() {
+      return examResult;
+    }
+    public float getPtile(){
+      return ptile;
+    }
+    public void setPtile(float n) {
+      this.ptile=n;
+    }
+  }
+  //comparator to sort students based on their marks
+  static class StudentMarksComparator implements Comparator<Student> {
+    @Override
+    public int compare(Student s1, Student s2) {
+      return s2.getMarks() - s1.getMarks();
+    }
   }
 }
