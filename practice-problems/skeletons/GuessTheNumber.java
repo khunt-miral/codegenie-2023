c6f90ebc045608fbc76018b9bf95a3e6cacffaa2
Khunt Miral <20ce051.miral.khunt@vvpedulink.ac.in>
2023-03-02 18:54:37 +0530

    GuessTheNumber done

--- practice-problems/skeletons/GuessTheNumber.java
+++ practice-problems/skeletons/GuessTheNumber.java
@@ -21,16 +21,32 @@
  */
 
 public class GuessTheNumber {
-  public static void main(String[] args) {
-    int finalNumber = 10;
-    String[] operations = {"X * 5", "X * 0", "X + 10"};
+  public static void main(String[] args) throws Exception {
+    int finalNumber =  617283948;
+    String[] operations = {"X + 5", "X - 0", "X + 1", "X / 2", "X ^ 1"};
 
     int actualNumber = GuessTheNumber.getActualNumber(finalNumber, operations, operations.length);
     System.out.println("The actual number will be " + actualNumber + " after performing given operations.");
   }
 
   public static int getActualNumber(int finalNumber, String[] operations, int totalOperations) throws Exception{
-    //TODO: Implement this function and remove below line, which throws exception
-    throw new RuntimeException("Not Implemented");
+    int num = finalNumber;
+    for(int i = totalOperations-1; i >= 0; i--) {
+      char opr = operations[i].charAt(2);
+      int op = Integer.parseInt(operations[i].substring(4));
+      //divide by 0 case
+      if(opr == '/' && op == 0) return -1;
+      //if multiple answers are possible then return -2
+      if(opr == '%'|| (opr=='^' && op == 0) || (opr == '*' && op == 0)) return -2;
+      switch (opr) {
+        case '+' -> num -= op;
+        case '-' -> num += op;
+        case '*' -> num /= op;
+        case '/' -> num *= op;
+        case '^' -> num = (int) Math.pow(num, 1f / op);
+      }
+    }
+
+    return num;
   }
 }
